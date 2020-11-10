c940f260028af912f89a6f59aefac9186ec2220f
shabad22 <shabad22@gmail.com>
2020-11-10 22:05:53 +0530

    updating crawler

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request
 from flask.templating import render_template
-from script_scrapper2 import crawler
+from script_scrapper2 import crawler, crawl_links
 from nltk import sent_tokenize
 import nltk
 
@@ -12,11 +12,10 @@ nltk.download('punkt')
 def home():
     if request.method == 'POST':
         link = request.form['link']
-        print(link)
+        if link == 'link':
+            link = request.form['choose']
         pa_out, en_out = crawler(link)
         en_out = sent_tokenize(en_out)
-        return render_template('input.html', pa_out=pa_out, en_out=en_out)
-    return render_template('input.html', pa_out='', en_out='')
-
-
-# app.run()
+        web_links = crawl_links(link)
+        return render_template('input.html', link='link', web_links=web_links, pa_out=pa_out, en_out=en_out)
+    return render_template('input.html', link='none', pa_out='', en_out='', web_links=['none'])
--- script_scrapper2.py
+++ script_scrapper2.py
@@ -21,36 +21,15 @@ def crawler(link, user_choice='n'):
     soup = BeautifulSoup(data.content, 'html.parser')
     data_list = []
     # user_choice = input(f'Want to explore all links in link {link}?("y/n") : ')
-    if user_choice == 'y':
-        data_links = [link for link in soup.find_all('a', href=True)]
-        data_links.append(link)
-        i = 0
-        for link in data_links:
-            try:
-                data = requests.get(link['href'])
-                soup = BeautifulSoup(data.content, 'html.parser')
-                tags = [tag.name for tag in soup.find_all()]
-                print('Crawling ', link['href'])
-                tags = list(set(tags))
-                i += 1
-                if i == 15:
-                    break
-                for tag in tags:
-                    for info in soup.find_all(tag):
-                        text_data = info.text
-                        data_list.append(text_data)
-
-            except:
-                print(f'Unable to connect with ', link['href'])
-                continue
-    else:
-        data = requests.get(link)
-        soup = BeautifulSoup(data.content, 'html.parser')
-        tags = [tag.name for tag in soup.find_all()]
-        for tag in tags:
-            for inf in soup.find_all('p'):
-                text_data = inf.text
-                data_list.append(text_data)
+    data_links = [link['href'] for link in soup.find_all('a', href=True)]
+    data_links.append(link)
+    data = requests.get(link)
+    soup = BeautifulSoup(data.content, 'html.parser')
+    tags = list(set([tag.name for tag in soup.find_all()]))
+    for tag in tags:
+        for inf in soup.find_all(tag):
+            text_data = inf.text
+            data_list.append(text_data)
     str_data = tokenizer(' '.join([i for i in data_list]))
     data_list = list(set(str_data.split('\n')))
     for line in data_list:
@@ -67,6 +46,22 @@ def crawler(link, user_choice='n'):
     # else:
     #     en_out.write(line+'\n')
     # pa_out.write('\n'.join([i for i in data_list]))
+
+
+def crawl_links(link):
+    # sending get request to link
+    data = requests.get(link)
+    # Getting content and parse html data
+    soup = BeautifulSoup(data.content, 'html.parser')
+    data_links = []
+    # user_choice = input(f'Want to explore all links in link {link}?("y/n") : ')
+    for i in soup.find_all('a', href=True):
+        if i['href'].startswith('h'):
+            data_links.append(i['href'])
+        else:
+            data_links.append(link+i['href'])
+    data_links.append(link)
+    return data_links
 
 
 if __name__ == "__main__":
